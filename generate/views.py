f971c5cd6edd0c6cc1f14e60ae081162b8859ac9
ahmed-humayun <93235542+ahmed-humayun@users.noreply.github.com>
2022-01-11 23:59:03 -0500

    added comments

--- generate/views.py
+++ generate/views.py
@@ -10,19 +10,27 @@ def about(request):
     return render(request, 'generate/about.html')
 
 def password(request):
+    
     characters = list('abcdefghijklmnopqrstuvwxyz')
+    # a list named characters which contains the lowercase letters
 
-    if request.GET.get('uppercase'):
+    if request.GET.get('uppercase'): 
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        # if uppercase checkbox is checked, uppercase letters are included in characters list
     if request.GET.get('special'):
         characters.extend(list('!@#$%^&*()'))
+        # if special character checkbox is checked, special letters are included in characters list 
     if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
+        # if numbers checkbox is checked, numbers  are included in characters list
 
-    length = int(request.GET.get('length',12))
+    length = int(request.GET.get('length',12)) 
+    # gets the desired length for password; default is 12
 
-    thepassword = ''
+    thepassword = '' #set the password to an empty string
     for x in range(length):
         thepassword += random.choice(characters)
+        #loops through the characters list and inserts a random character to the password
 
-    return render(request, 'generate/password.html', {'password':thepassword})
+    return render(request, 'generate/password.html', {'password':thepassword}) 
+    # returns the password to the password.html page
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from generate import views
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('password/', views.password, name='password'),
-    path('about/', views.about, name='about'),
+    path('', views.home, name='home'), #home.html
+    path('password/', views.password, name='password'), #password.html
+    path('about/', views.about, name='about'), #about.html
 ]
